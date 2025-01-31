e089571cc598a9abbf87ec7e7746daee625982fe
Saúlsop <sauljesusperezflores@gmail.com>
2025-01-30 19:51:34 -0600

    toast agregado

--- app/src/main/java/com/example/formulariocontacto/MainActivity.java
+++ app/src/main/java/com/example/formulariocontacto/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.formulariocontacto;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declarar las vistas
+    EditText txtNombre;
+    EditText txtTelefono;
+    EditText txtMensaje;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inicializar las vistas
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtMensaje = findViewById(R.id.txtMensaje);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        // Configurar el clic del botón "Enviar"
+        btnEnviar.setOnClickListener(v-> {
+
+                // Obtener los valores ingresados por el usuario
+                String nombre = txtNombre.getText().toString();
+                String telefono = txtTelefono.getText().toString();
+                String mensaje = txtMensaje.getText().toString();
+
+                // Mostrar un Toast con la información
+                String mensajeToast = "Nombre: " + nombre + "\nTeléfono: " + telefono + "\nMensaje: " + mensaje;
+                Toast.makeText(MainActivity.this, mensajeToast, Toast.LENGTH_LONG).show();
+
         });
     }
 }
